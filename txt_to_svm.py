554e70783895292d4d25f64d321d69e2925a2b15
popshia <popshia@cycu.org.tw>
2021-10-28 16:24:53 +0800

    finished and add report for the hw.

--- txt_to_svm.py
+++ txt_to_svm.py
@@ -2,6 +2,9 @@ import os
 
 def Read_Split_And_Write(input_dir, label, rgb_output, gradient_output):
     for file in sorted(list(filter(lambda x: '.txt' in x, os.listdir(input_dir)))):
+        rgb_output.write(label + " ")
+        gradient_output.write(label + " ")
+
         with open(os.path.join(input_dir, file), 'r') as input:
             rgb_count, gradient_count = 1, 1
 
@@ -11,9 +14,6 @@ def Read_Split_And_Write(input_dir, label, rgb_output, gradient_output):
                 for feature_value in line.split(' '):
                     if feature_value != "\n":
                         feature_count += 1
-
-                rgb_output.write(label + " ")
-                gradient_output.write(label + " ")
 
                 for times in range(int(feature_count / 5)):
                     rgb_output.write( str(rgb_count) + ":" + line.split( ' ' )[ times*5 ] + " " )
@@ -35,8 +35,8 @@ def Read_Split_And_Write(input_dir, label, rgb_output, gradient_output):
 
 if __name__ == '__main__':
     parent_dir = os.path.join(os.getcwd(), 'train')
-    rgb_output = open(os.path.join(parent_dir, 'rgb_scale'), 'a')
-    gradient_output = open(os.path.join(parent_dir, 'gradient_scale'), 'a')
+    rgb_output = open(os.path.join(parent_dir, 'rgb_features'), 'a')
+    gradient_output = open(os.path.join(parent_dir, 'gradient_features'), 'a')
 
     input_dir = os.path.join(os.getcwd(), 'train', 'heart')
     Read_Split_And_Write(input_dir, '+1', rgb_output, gradient_output)
@@ -44,8 +44,8 @@ if __name__ == '__main__':
     Read_Split_And_Write( input_dir, '-1', rgb_output, gradient_output )
 
     parent_dir = os.path.join(os.getcwd(), 'test')
-    rgb_output = open(os.path.join(parent_dir, 'rgb_scale'), 'a')
-    gradient_output = open(os.path.join(parent_dir, 'gradient_scale'), 'a')
+    rgb_output = open(os.path.join(parent_dir, 'rgb_features'), 'a')
+    gradient_output = open(os.path.join(parent_dir, 'gradient_features'), 'a')
 
     input_dir = os.path.join(os.getcwd(), 'test', 'heart')
     Read_Split_And_Write( input_dir, '+1', rgb_output, gradient_output )
